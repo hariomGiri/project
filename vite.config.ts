350ba1294c075b10d4da38346eebc89353b6ff78
Hariom giri <sureshgiri@gmail.com>
2025-03-04 19:08:17 +0530

    Adjust Vite configuration and index.html for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,11 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   },
 });
